e724208b86ae62f7ec1df33ae38eae6095abc414
piersonlinde <plinde1@gmail.com>
2024-10-03 19:44:42 -0600

    Make myStuff look good

--- obj.js
+++ obj.js
@@ -3,5 +3,5 @@ var myStuff = {
     "Birth Place": {city: "Atlanta", state: "Georgia", "coord": [-84.412595, 33.765545]},
     "Hometown": {city: "Cheyenne", state: "Wyoming", "coord": [-104.80999, 41.14638]},
     "Fishing Trip": {city: "Juneau", state: "Alaska", "coord": [-134.462813, 58.499564] },
-    "Volcano National Park": {city: "Island Of Hawai'i", state: "Hawaii", "coord": [-155.228528, 19.324689] }
+    "Dream Vacation: <br> Volcano National Park": {city: "Island Of Hawai'i", state: "Hawaii", "coord": [-155.228528, 19.324689] }
 }
