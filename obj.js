fb6eeff3989babf23cc9492eddffce5597536d40
piersonlinde <plinde1@gmail.com>
2024-10-06 19:23:52 -0600

    Update Alaska

--- obj.js
+++ obj.js
@@ -2,6 +2,6 @@ var myStuff = {
     "Graduate School": {city: "Laramie", state: "Wyoming", "coord": [-105.598927, 41.306380] },
     "Birth Place": {city: "Atlanta", state: "Georgia", "coord": [-84.412595, 33.765545]},
     "Hometown": {city: "Cheyenne", state: "Wyoming", "coord": [-104.80999, 41.14638]},
-    "Fishing Trip": {city: "Juneau", state: "Alaska", "coord": [-134.462813, 58.499564] },
+    "Fishing Trip": {city: "Juneau", state: "Alaska", "coord": [ -134.427902, 58.3067442] },
     "Dream Vacation: <br> Volcano National Park": {city: "Island Of Hawai'i", state: "Hawaii", "coord": [-155.228528, 19.324689] }
 }
